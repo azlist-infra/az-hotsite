b227c617143c462abff5c1fe998876bcade0489f
wallacemaxters <wallacemaxters@gmail.com>
2024-01-20 13:53:01 -0300

    ajustando os locais

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [
+    './index.html'
+  ],
   corePlugins: {
     container: false,
   },

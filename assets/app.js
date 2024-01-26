8239dd0a633773399e515e8686e353d14ecff31b
wallacemaxters <wallacemaxters@gmail.com>
2024-01-25 21:44:51 -0300

    Ligando os dados cadastrais ao back

--- assets/app.js
+++ assets/app.js
@@ -18,5 +18,55 @@ document.addEventListener('alpine:init', () => {
             gerarAgenda()
         ]
     }))
+
+
+    Alpine.data('cadastro', () => ({
+
+        pax: {
+            Name: '',
+            Email: '',
+            Phone: '',
+            Cpf: '',
+        },
+
+        token: null,
+
+        init() {
+
+            const url = new URL(window.location.href);
+            const id = url.searchParams.get('id')
+            this.token = url.searchParams.get('token');
+
+            fetch(`https://api-rds-aztools.onrender.com/api/magic-link/${id}?token=${this.token}`, {
+                method: 'GET',
+            }).then(async (res) => {
+                const data = await res.json();
+
+                this.pax = data.pax;
+            })
+        },
+
+        async submit () {
+
+            const response = await fetch(`https://api-rds-aztools.onrender.com/api/putMagicLink/${this.pax.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({
+                    Name: this.pax.Name,
+                    Cpf: this.pax.Cpf.replace(/\D+/g, ''),
+                    Phone: this.pax.Phone.replace(/\D/g, ''),
+                    Email: this.pax.Email,
+                    Token: this.token
+                }),
+            })
+
+            if (response.status === 200) {
+                alert('Seus dados foram atualizados com sucesso!')
+            }
+        }
+
+    }))
 })
 
